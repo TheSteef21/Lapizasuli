b31f03c7d0dfa3fb9e1e77852a76edb71b72b477
TheSteef21 <31745159+TheSteef21@users.noreply.github.com>
2026-02-18 18:58:08 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // El Verbo hecho estático
-  trailingSlash: true, // Para que el camino sea recto
+  output: 'export',      // Este es el sello que crea la carpeta de salida
+  trailingSlash: true,   // Esto asegura que tus links no se rompan en IPFS
+  distDir: '0x41',       // Aquí le decimos que la carpeta se llame como tu ley
+  images: {
+    unoptimized: true,   // Vital para que las imágenes se vean en el IPFS
+  },
 }
 
 module.exports = nextConfig
