e96705d6e57b66fda73a23bc8b7f3285e022a9fa
TheSteef21 <31745159+TheSteef21@users.noreply.github.com>
2026-02-18 17:29:41 -0500

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // El Verbo hecho estático
+  trailingSlash: true, // Para que el camino sea recto
+}
+
+module.exports = nextConfig
