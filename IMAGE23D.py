8b65bebea2026864a655cae01a40fd661d2a49d3
TheSteef21 <31745159+TheSteef21@users.noreply.github.com>
2026-06-15 20:24:53 -0500

    Create IMAGE23D.py

---
+++ IMAGE23D.py
@@ -0,0 +1,7 @@
+# Estructura básica de inferencia con TripoSR
+from tsr.system import TSR
+import torch
+
+model = TSR.from_pretrained("stabilityai/TripoSR", torch_dtype=torch.float16)
+model.to("cuda") # Lo corres en tu tarjeta gráfica
+# Luego pasas la imagen y exportas el archivo .obj o .glb
